389259cc99867e3ce6f4dfde0c177d36009dcf44
danlosdol <164674778+danilosdol@users.noreply.github.com>
2024-03-24 02:01:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-cc9f7339-2b61-4218-8ef6-fe6f2ae0edbc/)
 
 
 echo "$curl_output" >> Update.tsx
